b3cd7373ca570307572dcd3138a5462cd4e97cc4
Godwin-Godwin <godwinemmanuel302@gmail.com>
2024-02-19 17:25:20 +0100

    Add food menu to homepage

--- components/FoodMenu/index.tsx
+++ components/FoodMenu/index.tsx
@@ -4,26 +4,60 @@ import Image from "next/image";
 import "./index.scss";
 import { formatFoodName } from "@/utils/helpers";
 import { menu } from "@/data/data.json";
+import { PlusCircle } from "lucide-react";
+
+interface Dish {
+  name: string;
+  picture: string;
+  price: number;
+}
 
 const FoodMenu = () => {
   return (
     <div className="food-menu">
-      {/* {menu.Burger.map((food: any, index) => {
-        return (
-          <div key={index} className="food-card">
-            <Image
-              src={food.picture}
-              alt={food.name}
-              width={100}
-              height={100}
-            />
-            <p className="food-name">
-              <span>{formatFoodName(food.name)}</span>
-            </p>
-          </div>
-        );
-      })} */}
-      Our Menu
+      <h1>Our Special Menu</h1>
+      <div className="menu-box">
+        {Object.keys(menu).map((foodCategory) => {
+          return menu[foodCategory as keyof typeof menu].map(
+            (food: Dish, index: number) => {
+              return (
+                <div key={index} className="food-card">
+                  <div className="dish-image">
+                    <Image src={food.picture} alt={food.name} fill />
+                  </div>
+                  <div className="dish-infos">
+                    <div className="name-and-price">
+                      <small>{formatFoodName(food.name)}</small>
+                      <small>${food.price}</small>
+                    </div>
+                    <button>
+                      <PlusCircle size={18} /> ADD TO CART
+                    </button>
+                  </div>
+                </div>
+              );
+            }
+          );
+        })}
+        {/* {menu.Burger.map((food: any, index) => {
+          return (
+            <div key={index} className="food-card">
+              <div className="dish-image">
+                <Image src={food.picture} alt={food.name} fill />
+              </div>
+              <div className="dish-infos">
+                <div className="name-and-price">
+                  <small>{formatFoodName(food.name)}</small>
+                  <small>${food.price}</small>
+                </div>
+                <button>
+                  <PlusCircle size={18} /> ADD TO CART
+                </button>
+              </div>
+            </div>
+          );
+        })} */}
+      </div>
     </div>
   );
 };
